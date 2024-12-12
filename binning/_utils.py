cebaf44170158369dbf8742678289bbc5c240733
BenceLazarovits <s7885laz@gmail.com>
2024-12-12 12:01:42 +0100

    fits and transforms decorated with pandas

--- binning/_utils.py
+++ binning/_utils.py
@@ -20,5 +20,7 @@ def pandizator_decorator_inout(func):
             series_name = args[1].name
             tmp = args[1].values
             args = (args[0], tmp, *args[2:])
-        return pd.Series(func(*args, **kwargs), name=f'{series_name}_binned')
+            return pd.Series(func(*args, **kwargs), name=f'{series_name}_binned')
+        else:
+            return func(*args, **kwargs)
     return inner
